6180b09d790a6c2c8f86cf7fecb06c65c6c52066
chessgrandest-prog <chessgrandest@gmail.com>
2026-01-14 19:23:26 -0600

    Fix URLMeta shape for Cloudflare build

--- functions/_worker.js
+++ functions/_worker.js
@@ -1,7 +1,5 @@
 import server from "../dist/server.js";
 
-export default {
-  async fetch(request, env, ctx) {
-    return server.fetch(request, env, ctx);
-  }
-};
+export async function onRequest(context) {
+  return server.fetch(context.request, context.env, context);
+}
